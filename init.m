31fdb8702270c731cb39c8e355006a9c1510d393
Alexander Junjie Zeng <alexanderj.j.zeng@gmail.com>
2026-07-26 14:26:26 +0800

    Update init.m

--- init.m
+++ init.m
@@ -2,12 +2,6 @@
 
 (** User Mathematica initialization file **)
 
-(*Block[{kernelnumber, machinenames = {"desktop-p24f3e7"}},
-	kernelnumber = If[MemberQ[$MachineName][machinenames], 20, 2 $ProcessorCount];
-	LaunchKernels[kernelnumber];
-	Echo[StringTemplate["The launched kernel number is: ``."][$KernelCount]];
-];*)
-
 LaunchKernels[20];
 Echo[StringTemplate["The launched kernel number is: ``."][$KernelCount]];
 Needs["TBMethod`"]
